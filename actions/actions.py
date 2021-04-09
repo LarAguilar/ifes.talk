d7c34afa0d5b80132ab1eab7183884d3f390d5ed
Lara Aguilar <72893552+LarAguilar@users.noreply.github.com>
2021-04-08 22:17:14 -0300

    intent começar conversa
    Acrescentei a custom action que da uma resposta especifica quando o usuario digita o comando /session_start

--- actions/actions.py
+++ actions/actions.py
@@ -1,7 +1,21 @@
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import SlotSet
+from rasa_sdk.events import SlotSet, SessionStarted, ActionExecuted, EventType
+
+#inicio sessao
+class ActionSessionStart(Action):
+    def name(self) -> Text:
+        return "action_session_start"
+
+    async def run(
+      self, dispatcher, tracker: Tracker, domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+
+        # the session should begin with a `session_started` event
+        events = [SessionStarted()]
+
+        return events
 
 ## Função que de acordo com o intent anterior do usuário, pega seu campus, e manda uma resposta junto do link do calendário acadêmico do site do seu campus
 class ActionLinkCampus(Action):
